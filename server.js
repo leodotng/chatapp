f33310364736254c386c03ec7b6ccb847e39b35c
leodotng <josh@leo.ng>
2018-12-13 23:50:07 -0700

    User Connected status

--- server.js
+++ server.js
@@ -8,3 +8,22 @@ const server = require('http').Server(app);
 io = require('socket.io')(server);
 users={};
 server.listen(3000);
+
+io.sockets.on('connection',function(socket){
+    console.log("User Connected");
+socket.on('new', function(data,callback){
+    console.log(data.name);
+    if(data in users)
+    callback(false);
+    else
+    {
+    callback(true);
+    socket.name = data.name;
+    users[socket.name]=socket;
+    }
+})
+socket.on('msg',function(data,callback){
+    callback(data.msg);
+    io.to(users[data.to].emit('priv',data.msg));
+})
+})
